58ae1568eb332c95e9699e76c638b2493deee05c
Peter Neish <peter.neish@gmail.com>
2018-01-15 14:23:02 +1100

    adding sql for table creation

---
+++ data/create_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE reponse (
+	id bigserial primary key,
+	date_added timestamp default NOW(),
+	country varchar(100),
+	org_type varchar(30),
+	org_size varchar(30),
+	q1 int,q2 int,q3 int,q4 int,q5 int,q6 int,q7 int,q8 int,q9 int,q10 int,
+	q11 int,q12 int,q13 int,q14 int,q15 int,q16 int,q17 int,q18 int,q19 int,q20 int,
+	q21 int,q22 int,q23 int,q24 int,q25 int,q26 int,q27 int,q28 int,q29 int,q30 int,
+	q31 int,q32 int,q33 int,q34 int,q35 int,q36 int,q37 int,q38 int,q39 int,q40 int,
+	q41 int,q42 int,q43 int,q44 int,q45 int,q46 int,q47 int,q48 int,q49 int,q50 int,
+	q51 int,q52 int,q53 int,q54 int,q55 int,q56 int,q57 int,q58 int,q59 int,q60 int,
+	q61 int,q62 int,q63 int,q64 int,q65 int,q66 int,q67 int,q68 int,q69 int,q70 int,
+	q71 int,q72 int,q73 int,q74 int,q75 int,q76 int,q77 int,q78 int,q79 int,q80 int,
+	q81 int,q82 int,q83 int,q84 int,q85 int,q86 int,q87 int,q88 
+);
