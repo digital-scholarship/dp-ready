abebb7074ddb1e6adfadf34246c76a12937e5f5f
Peter Neish <peter.neish@gmail.com>
2018-01-18 23:02:28 +1100

    simplifing postgres calls with helpers

--- data/create_table.sql
+++ data/create_table.sql
@@ -1,9 +1,10 @@
-CREATE TABLE reponse (
+CREATE TABLE response (
 	id bigserial primary key,
+	uid text,
 	date_added timestamp default NOW(),
-	country varchar(100),
-	org_type varchar(30),
-	org_size varchar(30),
+	country text,
+	org_type text,
+	org_size text,
 	q1 int,q2 int,q3 int,q4 int,q5 int,q6 int,q7 int,q8 int,q9 int,q10 int,
 	q11 int,q12 int,q13 int,q14 int,q15 int,q16 int,q17 int,q18 int,q19 int,q20 int,
 	q21 int,q22 int,q23 int,q24 int,q25 int,q26 int,q27 int,q28 int,q29 int,q30 int,
@@ -12,5 +13,5 @@ CREATE TABLE reponse (
 	q51 int,q52 int,q53 int,q54 int,q55 int,q56 int,q57 int,q58 int,q59 int,q60 int,
 	q61 int,q62 int,q63 int,q64 int,q65 int,q66 int,q67 int,q68 int,q69 int,q70 int,
 	q71 int,q72 int,q73 int,q74 int,q75 int,q76 int,q77 int,q78 int,q79 int,q80 int,
-	q81 int,q82 int,q83 int,q84 int,q85 int,q86 int,q87 int,q88 
+	q81 int,q82 int,q83 int,q84 int,q85 int,q86 int,q87 int,q88 int 
 );
--- queries.js
+++ queries.js
@@ -1,8 +1,7 @@
 var pgp = require('pg-promise')();
 var connectionString = process.env.DATABASE_URL;
-var db = pgp(connectionString);
+var db = pgp(connectionString); // 'postgres://cessda@localhost/dpready'
 
-// add query functions
 
 function getAllResponses(req, res, next) {
   db.any('select * from response')
@@ -21,7 +20,7 @@ function getAllResponses(req, res, next) {
 
 function getSingleResponse(req, res, next) {
   var resID = parseInt(req.params.id);
-  db.one('select * from response where id = $1', resID)
+  db.one('select * from response where uid = $1', resID)
     .then(function (data) {
       res.status(200)
         .json({
@@ -36,8 +35,21 @@ function getSingleResponse(req, res, next) {
 }
 
 function createResponse(req, res, next) {
-    // stub
-   }
+
+    var insert = pgp.helpers.insert(req.body, null, 'response');
+
+    db.none(insert)
+    .then(function(){
+        res.status(200)
+            .json({
+                status: 'success',
+                message: 'inserted one response'
+            });
+    })
+    .catch(function(err){
+        return next(err);
+    });
+}
 
 function updateResponse(req, res, next) {
     // stub
@@ -46,6 +58,12 @@ function updateResponse(req, res, next) {
 function getResponsesBy(req, res, next) {
     // stub
    }
+
+
+// returns a pretty much unique hash
+function getUid(){
+
+}
 module.exports = {
   getAllResponses: getAllResponses,
   getSingleResponse: getSingleResponse,
